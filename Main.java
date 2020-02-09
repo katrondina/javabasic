81d604d2c5b30bb73a9d12c61f2dfc3b1a97d21a
Kat Rondina - s340 <katrondina@hotmail.com>
2020-02-09 19:16:54 +0800

    newjavabasic

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] arg){
+        System.out.println("Hellow World, this is Katzi");
+        int j=10;
+        for(int i=1; i<=j;i++){
+            System.out.println("Number " + i);
+        }
+    }
+}
